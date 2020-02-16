789a1d0a01b4b4be3d8076ea1fd0143d42b35b4e
inf <inf@infs-MacBook-Pro.local>
2020-02-16 18:44:57 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function ($) { 
+    "use strict";
+
+    alert('eee');
+
+    $(window).on('load', function() { 
+    // Custom Loading page
+    $(".main-technology-setup-code").delay(1000).fadeOut();
+    //   $("#loader").css('display', 'none !important');
+    // $("#loaderInner img").removeClass("loading");
+    // $("#loaderInner p").removeClass("loading");
+    });
+
+})(jQuery);
